9e92a718c5b33fee024e646e953691f17babfe02
nanditav17@gmail.com <nandita@panda.(none)>
2014-10-31 18:51:48 -0400

    adding env template

---
+++ set_env_template.sh
@@ -0,0 +1,12 @@
+export GPGPUSIM_ROOT=/home/nandita/working_dir/gpgpu-sim
+export CUDAHOME=/usr/old_local/cuda/cuda
+export LD_LIBRARY_PATH=$CUDAHOME/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUDAHOME/lib64/:$LD_LIBRARY_PATH:/usr/local/cuda/lib/:
+export LD_LIBRARY_PATH=$GPGPUSIM_ROOT/lib/4000/release:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/nandita/working_dir/gpgpu-sim/lib/4000/release:/home/nandita/working_dir/gpgpu-sim/lib/4000/release:/usr/old_local/cuda/cuda/lib64/:/usr/old_local/cuda/cuda/lib/:/home/nandita/working_dir/gpgpu-sim/lib/4000/release:/usr/old_local/cuda/cuda/lib64/:/usr/old_local/cuda/cuda/lib/:/usr/local/cuda/lib:/usr/local/cuda/lib:
+export CUDA_INSTALL_PATH=/usr/old_local/cuda/cuda
+export NVIDIA_CUDA_SDK_LOCATION=/home/nandita/NVIDIA_GPU_Computing_SDK
+export GPGPUSIM_POWER_MODEL=$GPGPUSIM_ROOT/src/gpuwattch
+export PATH=$CUDAHOME/bin/:$PATH
+export BOOST_ROOT=/home/nandita/boost_1_54_0
+export PARBOIL_ROOT=/home/nandita/working_dir/gpgpu-sim/benchmarks/parboil
